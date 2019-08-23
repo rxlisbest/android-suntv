704df0355eb2ffb2df91a3f648643dc833b5e0ea
Roy <rxlisbest@gmail.com>
2019-08-23 14:30:09 +0800

    bilibili播放器

--- app/src/main/java/net/ruixinglong/suntv/MainActivity.java
+++ app/src/main/java/net/ruixinglong/suntv/MainActivity.java
@@ -14,37 +14,78 @@
 
 package net.ruixinglong.suntv;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-import com.shuyu.gsyvideoplayer.GSYVideoManager;
-import com.shuyu.gsyvideoplayer.model.VideoOptionModel;
-import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
-import com.shuyu.gsyvideoplayer.video.base.GSYVideoView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 /*
  * Main Activity class that loads {@link MainFragment}.
  */
-public class MainActivity extends Activity {
-
-    private StandardGSYVideoPlayer videoPlayer;
+public class MainActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    private IjkMediaPlayer mPlayer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<VideoOptionModel> list = new ArrayList<>();
-        GSYVideoManager.instance().setOptionModelList(list);
-        String source1 = "rtsp://192.168.1.101/11";
-        videoPlayer.setUp(source1, true, "");
-        videoPlayer.getTitleTextView().setVisibility(View.GONE);
-        videoPlayer.startPlayLogic();
+        surfaceView = findViewById(R.id.surface_view);
+        surfaceView.getHolder().addCallback(callback);
+    }
+
+    private void createPlayer() {
+        if (mPlayer == null) {
+            mPlayer = new IjkMediaPlayer();
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mPlayer.setDataSource("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mPlayer.prepareAsync();
+        }
+    }
+
+    private void release() {
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+        IjkMediaPlayer.native_profileEnd();
+    }
+
+    private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            createPlayer();
+            mPlayer.setDisplay(surfaceView.getHolder());
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (surfaceView != null) {
+                surfaceView.getHolder().removeCallback(callback);
+                surfaceView = null;
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        release();
     }
 }
